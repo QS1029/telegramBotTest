b52caaa490f7e9f7153c7e196c8edcf87445405c
Cybersport <Cybersport@26kadr.internal>
2024-05-21 14:52:27 +0300

    fifth

--- keyboard/key_inline.py
+++ keyboard/key_inline.py
@@ -1,6 +1,20 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def get_keyboard_inline():
+def get_keyboard_start1():
+    keyboard_main = InlineKeyboardMarkup(row_width=1)
+    button1 = InlineKeyboardButton("Go to menu 2", callback_data = "go_to_2")
+    button2 = InlineKeyboardButton("Send me a random number (If I roll a 6 you fucking die)", callback_data="send_random_number")
+    keyboard_main.add(button1, button2)
+    return keyboard_main
+
+def get_keyboard_start2():
+    keyboard_main2 = InlineKeyboardMarkup(row_width=1)
+    button3 = InlineKeyboardButton("Go to menu 1", callback_data = "go_to_1")
+    button4 = InlineKeyboardButton("What time is it (im deranged i cant look it up myself)", callback_data="send_datetime")
+    keyboard_main2.add(button3, button4)
+    return keyboard_main2
+
+def get_keyboard_inline(): #for the first keyboard
     keyboard_inline = InlineKeyboardMarkup(row_width=2)
     but_inline = InlineKeyboardButton("Посмотреть", url="https://i.ytimg.com/vi/QlChyW_6kYA/maxresdefault.jpg")
     but_inline_2 = InlineKeyboardButton("Что-то", url="https://patents.google.com/patent/US20090292143A1/en")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, Dispatcher,types, executor
 from config import TELEGRAM_TOKEN
 from keyboard.keyboards import get_keyboard_1, get_keyboard_2, get_keyboard_3
-from keyboard.key_inline import get_keyboard_inline
+from keyboard.key_inline import get_keyboard_inline, get_keyboard_start1, get_keyboard_start2
+import random
+from datetime import datetime
 
 bot = Bot(token = TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
@@ -15,51 +17,78 @@ async def set_commands(bot: Bot):
     ]
     await bot.set_my_commands(commands)
 
-#START
+#START MENU
 @dp.message_handler(commands = 'start')
 async def start(message: types.Message):
-    await message.reply('hiiiiii', reply_markup = get_keyboard_1())
+    await message.reply('hiiiiii welcome the main menu of this definetely good bot!!!', reply_markup = get_keyboard_start1())
 
-#KEYBOARD1--------------------------------
-#KITTY PIC
-@dp.message_handler(lambda message: message.text == "send me a kitty!11!11")
-async def button_1_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo = 'https://i.pinimg.com/originals/a4/24/ab/a424ab7e7e419a35a4f980eb5ed84921.jpg', caption = 'he is wanted in 15 countires across the world.', reply_markup = get_keyboard_inline())
+@dp.callback_query_handler(lambda c: c.data == 'send_random_number')
+async def random_number(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(f"uhhhh, here you can have this {random.randint(1,100)}")
 
-#PROCEED TO KEYBOARD2
-@dp.message_handler(lambda message: message.text == "Would. Next keyboard.")
-async def button_2_click(message: types.Message):
-    await message.answer("THE PUNCHLINE IS- Let us proceed to the next keyboard, fine gentleman.", reply_markup = get_keyboard_2())
-#-----------------------------------------
+@dp.callback_query_handler(lambda c: c.data == 'send_datetime')
+async def send_datetime(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(f"i uhhh its {datetime.now().strftime('%H:%M:%S')} right now")
 
-#KEYBOARD2--------------------------------
-#ANOTHER KITTY PIC
-@dp.message_handler(lambda message: message.text == "send me another kitty!!!11")
-async def button_3_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo = 'https://i.pinimg.com/originals/57/c3/e6/57c3e6125d790ea5db39c8b79c861414.jpg', caption = 'he commited 72 different crimes and isnt planning on stopping')
+@dp.callback_query_handler(lambda c: c.data == 'go_to_2')
+async def go_to_2(callback_query: types.CallbackQuery):
+    await callback_query.message.edit_text('Sentenced to be sealed away in keyboard 2 (Unseal yourself with that button fr)',
+                           reply_markup = get_keyboard_start2())
 
-#PROCEED TO KEYBOARD1
-@dp.message_handler(lambda message: message.text == "Would not. Previous keyboard.")
-async def button_4_click(message: types.Message):
-    await message.answer("ok yes i understand hmmm yes yes keyboard 1", reply_markup = get_keyboard_1())
+@dp.callback_query_handler(lambda c: c.data == 'go_to_1')
+async def go_to_1(callback_query: types.CallbackQuery):
+    await callback_query.message.edit_text('Sentenced to be sealed away in keyboard 1 (Unseal yourself with that button fr)',
+                            reply_markup=get_keyboard_start1())
 
-#PROCEED TO KEYBOARD3
-@dp.message_handler(lambda message: message.text == "Maybe would. Next keyboard.")
-async def button_5_click(message: types.Message):
-    await message.answer("ah i see a most excellent choice, keyboard 3 yes yes", reply_markup = get_keyboard_3())
-#-----------------------------------------
 
-#KEYBOARD3--------------------------------
-#YET ANOTHER KITTY PIC (its never enough)
-@dp.message_handler(lambda message: message.text == "send me more critters...")
-async def button_6_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo = 'https://i.ytimg.com/vi/QlChyW_6kYA/maxresdefault.jpg', caption = 'bro is so silly he got into the silly competition')
-
-#PROCEED TO KEYBOARD2
-@dp.message_handler(lambda message: message.text == "Def no. Previous keyboard.")
-async def button_5_click(message: types.Message):
-    await message.answer("understandable, returning to keyboard 2 hq", reply_markup = get_keyboard_2())
-#-----------------------------------------
+# STUFF BELOW THIS LINE ISNT BEING USED NOW (ILL KEEP IT ANYWAY)
+#
+# #KEYBOARD1
+# #-----------------------------------------
+#
+# #KITTY PIC
+# @dp.message_handler(lambda message: message.text == "send me a kitty!11!11")
+# async def button_1_click(message: types.Message):
+#     await bot.send_photo(message.chat.id, photo = 'https://i.pinimg.com/originals/a4/24/ab/a424ab7e7e419a35a4f980eb5ed84921.jpg',
+#                          caption = 'he is wanted in 15 countires across the world.', reply_markup = get_keyboard_inline())
+#
+# #PROCEED TO KEYBOARD2
+# @dp.message_handler(lambda message: message.text == "Would. Next keyboard.")
+# async def button_2_click(message: types.Message):
+#     await message.answer("THE PUNCHLINE IS- Let us proceed to the next keyboard, fine gentleman.", reply_markup = get_keyboard_2())
+# #-----------------------------------------
+#
+# #KEYBOARD2
+# #-----------------------------------------
+#
+# #ANOTHER KITTY PIC
+# @dp.message_handler(lambda message: message.text == "send me another kitty!!!11")
+# async def button_3_click(message: types.Message):
+#     await bot.send_photo(message.chat.id, photo = 'https://i.pinimg.com/originals/57/c3/e6/57c3e6125d790ea5db39c8b79c861414.jpg', caption = 'he commited 72 different crimes and isnt planning on stopping')
+# #PROCEED TO KEYBOARD1
+# @dp.message_handler(lambda message: message.text == "Would not. Previous keyboard.")
+# async def button_4_click(message: types.Message):
+#     await message.answer("ok yes i understand hmmm yes yes keyboard 1", reply_markup = get_keyboard_1())
+#
+# #PROCEED TO KEYBOARD3
+# @dp.message_handler(lambda message: message.text == "Maybe would. Next keyboard.")
+# async def button_5_click(message: types.Message):
+#     await message.answer("ah i see a most excellent choice, keyboard 3 yes yes", reply_markup = get_keyboard_3())
+# #-----------------------------------------
+#
+# #KEYBOARD3
+# #-----------------------------------------
+#
+# #YET ANOTHER KITTY PIC (its never enough)
+# @dp.message_handler(lambda message: message.text == "send me more critters...")
+# async def button_6_click(message: types.Message):
+#     await bot.send_photo(message.chat.id, photo = 'https://i.ytimg.com/vi/QlChyW_6kYA/maxresdefault.jpg', caption = 'bro is so silly he got into the silly competition')
+#
+# #PROCEED TO KEYBOARD2
+# @dp.message_handler(lambda message: message.text == "Def no. Previous keyboard.")
+# async def button_5_click(message: types.Message):
+#     await message.answer("understandable, returning to keyboard 2 hq", reply_markup = get_keyboard_2())
+# #-----------------------------------------
 
 #HELP
 @dp.message_handler(commands = 'help')
